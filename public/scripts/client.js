e02919e28024d7e14fceaaf38f582d55ee251cf9
Donatas Tubutis <dotubutis@gmail.com>
2022-02-02 17:28:50 +0000

    form validation; refactoring tweet submit ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,32 +6,32 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
+// const data = [
+//   {
+//     user: {
+//       name: "Newton",
+//       avatars: "https://i.imgur.com/73hZDYK.png",
+//       handle: "@SirIsaac",
+//     },
+//     content: {
+//       text: "If I have seen further it is by standing on the shoulders of giants",
+//     },
+//     created_at: 1461116232227,
+//   },
+//   {
+//     user: {
+//       name: "Descartes",
+//       avatars: "https://i.imgur.com/nlhLi3I.png",
+//       handle: "@rd",
+//     },
+//     content: {
+//       text: "Je pense , donc je suis",
+//     },
+//     created_at: 1461113959088,
+//   },
+// ];
 
-$(document).ready(function () {
+$(() => {
   const createTweetElement = ({ user, content, created_at }) => {
     const htmlMarkup = `
     <article class="tweet">
@@ -66,10 +66,30 @@ $(document).ready(function () {
     });
   };
 
-  renderTweets(data);
+  const loadTweets = () => {
+    $.ajax("/tweets", { method: "GET" }).then(function (moreTweetsJson) {
+      renderTweets(moreTweetsJson);
+    });
+  };
+
+  loadTweets();
 
   $("#newTweet").submit(function (event) {
     event.preventDefault();
-    $.post("/tweets", $("#newTweet").serialize());
+    const tweetLength = $("#tweet-text").val().length;
+    if (tweetLength > 140) {
+      alert("Sorry, tweet is too long!");
+      return;
+    }
+    if (tweetLength === 0) {
+      alert("Why post an empty tweet?");
+      return;
+    }
+    $.post("/tweets", $("#newTweet").serialize())
+      .then(() => {
+        alert("Tweet submited!");
+        $("#tweet-text").val("");
+      })
+      .catch(() => alert("Sorry, there was an error"));
   });
 });
