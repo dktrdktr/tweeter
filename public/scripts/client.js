39188d16bc78632a8a970fcfb4c87209486d0fff
Donatas Tubutis <dotubutis@gmail.com>
2022-02-02 19:03:43 +0000

    refetching tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-// Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     user: {
-//       name: "Newton",
-//       avatars: "https://i.imgur.com/73hZDYK.png",
-//       handle: "@SirIsaac",
-//     },
-//     content: {
-//       text: "If I have seen further it is by standing on the shoulders of giants",
-//     },
-//     created_at: 1461116232227,
-//   },
-//   {
-//     user: {
-//       name: "Descartes",
-//       avatars: "https://i.imgur.com/nlhLi3I.png",
-//       handle: "@rd",
-//     },
-//     content: {
-//       text: "Je pense , donc je suis",
-//     },
-//     created_at: 1461113959088,
-//   },
-// ];
-
 $(() => {
   const createTweetElement = ({ user, content, created_at }) => {
     const htmlMarkup = `
@@ -60,9 +33,11 @@ $(() => {
     // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
+    // $("#tweets-container").html("");
+    $("#tweets-container").empty();
     tweets.forEach((tweetObj) => {
       const $tweet = createTweetElement(tweetObj);
-      $("#tweets-container").append($tweet);
+      $("#tweets-container").prepend($tweet);
     });
   };
 
@@ -73,7 +48,6 @@ $(() => {
   };
 
   loadTweets();
-
   $("#newTweet").submit(function (event) {
     event.preventDefault();
     const tweetLength = $("#tweet-text").val().length;
@@ -89,6 +63,8 @@ $(() => {
       .then(() => {
         alert("Tweet submited!");
         $("#tweet-text").val("");
+        $(".counter").val("140");
+        loadTweets();
       })
       .catch(() => alert("Sorry, there was an error"));
   });
